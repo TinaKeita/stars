d486cacace1194002c5d3ea1369852136b10454a
ipb23.t.paegle <ipb23.t.paegle@pjvt.local>
2024-11-26 15:11:44 +0200

    izveidota poga

--- index.php
+++ index.php
@@ -8,13 +8,48 @@ $connection = new mysqli("localhost","root","","sipols"); //savienojums ar datu 
 $result = $connection->query("SELECT * FROM users"); //selecto datus no datu bazes query 
 $data = $result->fetch_assoc(); //paņem no datu bāzes pirmās rindiņas datus
 
-var_dump($data); //izvada visu 
-//var_dump($connection);
+print_r($data); //izvada visu 
+echo "<br><br>";
 
 //Ielikt lietotāju no HTML formas
+echo"<pre>";
+var_dump($_SERVER);
+echo"</pre>";
 
-echo "<form>";
+if($_SERVER["REQUEST_METHOD"] =="POST") {
+    $username = $_POST["usurname"];
+    $password = $_POST["passv"];
 
+    $sql = "INSERT INTO users
+            (username, pass)
+            VALUES
+            ('$username', '$password')";
+    
+    $connection->query($sql);
+};
+
+echo "<form method='POST'>";
+    echo "<label>Lietotājvārds: <input name='usurname' placeholder='Lietotājvārds'/></label><br><br>";
+    echo "<label>Parole: <input type='password' name='passv' placeholder='Parole'/></label><br><br>";
+    
+    echo "<input type='submit' value='Reģistrēties'/>";
 echo "</form>";
 
+
+
+
+
+
+//ir 2 masvu veidi
+//Izveido masīvu ar vērtībām "sarkans", "abols", 120
+//Indeksi tiek piešķirti automātiski
+/*echo "<br><br>";
+$auglis = ["sarkans", "ābols",120];
+print_r($auglis);
+
+//Pats iedod indeksus
+//Asociatīais masīvs color, type, weight
+$auglisAssoc = ["color" => "sarkans", "type" => "ābols", "weight" => 120];
+print_r($auglisAssoc);
+*/
 ?>
---
+++ script.js
@@ -0,0 +1,7 @@
+let mainigais = 0;
+function handleClick(){
+    console.log("Strada");
+    mainigais = mainigais + 1;
+    document.getElementById("poga").textContent = mainigais;
+}
+
